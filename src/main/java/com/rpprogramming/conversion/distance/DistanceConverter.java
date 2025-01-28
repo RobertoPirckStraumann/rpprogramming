afbd6566fe3dbf1aefaa2a1c4ff3b5b38f958cb1
Roberto Pirck <roberto.pirck@straumann.com>
2025-01-29 00:16:45 +0100

    Add javadoc comments

--- src/main/java/com/rpprogramming/conversion/distance/DistanceConverter.java
+++ src/main/java/com/rpprogramming/conversion/distance/DistanceConverter.java
@@ -2,19 +2,44 @@ package com.rpprogramming.conversion.distance;
 
 public class DistanceConverter {
 
+    /**
+     * Converts a distance from kilometers to miles.
+     *
+     * @param kilometers the distance in kilometers to be converted
+     * @return Distance in miles
+     */
     public static double kilometersToMiles(double kilometers) {
         return kilometers * 0.621371;
     }
 
+    /**
+     * Converts a distance from miles to kilometers.
+     *
+     * @param miles the distance in miles to be converted
+     * @return Distance in kilometers
+     */
     public static double milesToKilometers(double miles) {
         return miles * 1.60934;
     }
 
+    /**
+     * Converts a distance from feet to meters.
+     *
+     * @param feets the distance in feet to be converted
+     * @return Distance in meters
+     */
     public static double feetsToMeters(double feets) {
         return feets * 0.3048;
     }
 
+    /**
+     * Converts a distance from meters to feet.
+     *
+     * @param meters the distance in meters to be converted
+     * @return Distance in feet
+     */
     public static double metersToFeets(double meters) {
         return meters * 3.28084;
     }
+
 }
--- src/main/java/com/rpprogramming/conversion/temperature/TemperatureConverter.java
+++ src/main/java/com/rpprogramming/conversion/temperature/TemperatureConverter.java
@@ -2,10 +2,22 @@ package com.rpprogramming.conversion.temperature;
 
 public class TemperatureConverter {
 
+    /**
+     * Converts Celsius to Fahrenheit.
+     *
+     * @param celsius Temperature in Celsius.
+     * @return Temperature in Fahrenheit.
+     */
     public static double celsiusToFahrenheit(double celsius){
         return (celsius * 9.0 / 5.0) + 32;
     }
 
+    /**
+     * Converts Fahrenheit to Celsius.
+     *
+     * @param fahrenheit Temperature in Fahrenheit.
+     * @return Temperature in Celsius.
+     */
     public static double fahrenheitToCelsius(double fahrenheit){
         return (fahrenheit - 32) * 5.0 / 9.0;
     }
